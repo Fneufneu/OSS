4d8b65c9066d2e685a6a18d1f929bec6711f2166
Fneufneu <fneufneu@xbmc.org>
2011-04-05 17:25:24 +0200

    fix build for FreeBSD's OSS

--- ossinfo/ossinfo.c
+++ ossinfo/ossinfo.c
@@ -26,7 +26,11 @@
 #ifndef LOCAL_BUILD
 #include <local_config.h>
 #endif
+#ifdef HAVE_SYS_SOUNDCARD_H
+#include <sys/soundcard.h>
+#else
 #include <soundcard.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -122,23 +126,31 @@ print_verbose_formats (unsigned long mask)
     printf ("      AFMT_MPEG\t- MPEG (MP2/MP3) encoded audio\n");
   if (mask & AFMT_AC3)
     printf ("      AFMT_AC3\t\t- AC3 (Dolby Digital) encoded audio\n");
+#ifdef AFMT_VORBIS	
   if (mask & AFMT_VORBIS)
     printf ("      AFMT_VORBIS\t- Vorbis encoded audio\n");
+#endif	
   if (mask & AFMT_S32_LE)
     printf ("      AFMT_S32_LE\t- 32 bit signed little endian\n");
   if (mask & AFMT_S32_BE)
     printf ("      AFMT_S32_BE\t- 32 bit signed big endian\n");
+#ifdef AFMT_FLOAT
   if (mask & AFMT_FLOAT)
     printf
       ("      AFMT_FLOAT\t- Single precision floating point (native endianess)\n");
+#endif	
   if (mask & AFMT_S24_LE)
     printf ("      AFMT_S24_LE\t- 24/32 bit signed little endian\n");
   if (mask & AFMT_S24_BE)
     printf ("      AFMT_S24_BE\t- 24/32 bit signed big endian\n");
+#ifdef AFMT_S24_PACKED
   if (mask & AFMT_S24_PACKED)
     printf ("      AFMT_S24_PACKED\t- 24 bit packed (3 byte)\n");
+#endif	
+#ifdef AFMT_SPDIF_RAW
   if (mask & AFMT_SPDIF_RAW)
     printf ("      AFMT_SPDIF_RAW\t- Raw S/PDIF frames\n");
+#endif	
 }
 
 static void
@@ -158,15 +170,21 @@ print_engine_info (oss_audioinfo * ainfo, int num)
 
   switch (ainfo->busy)
     {
+#ifdef OSS_OPEN_READ
     case OSS_OPEN_READ:
       printf ("Busy (IN) ");
       break;
+#endif
+#ifdef OSS_OPEN_WRITE
     case OSS_OPEN_WRITE:
       printf ("Busy (OUT) ");
       break;
+#endif
+#ifdef OSS_OPEN_READWRITE
     case OSS_OPEN_READWRITE:
       printf ("Busy (IN/OUT) ");
       break;
+#endif
     default:
       printf ("Available for use ");
     }
@@ -251,9 +269,11 @@ print_audio_info (void)
       if (verbose == 0)
 	continue;
 
+#ifdef OSS4
       if (ainfo.legacy_device >= 0)
 	printf ("    Legacy device /dev/dsp%d\n", ainfo.legacy_device);
       else
+#endif
 	printf ("    Legacy device NONE\n");
 
       printf ("    Caps: ");
@@ -628,8 +648,10 @@ print_mixer_info (void)
       if (verbose == 0)
 	continue;
 
+#ifdef OSS4
       printf ("    Device file %s, Legacy device /dev/mixer%d\n",
 	      minfo.devnode, minfo.legacy_device);
+#endif
 
       /* Check if the device is currently busy or not */
       if (!minfo.enabled)
@@ -646,10 +668,14 @@ print_mixer_info (void)
       printf ("    Priority: %d\n", minfo.priority);
       printf ("    Caps: ");
       if (minfo.caps & MIXER_CAP_VIRTUAL)
+#ifdef MIXER_CAP_LAYOUT_B
 	printf ("VIRTUAL ");
       if (minfo.caps & MIXER_CAP_LAYOUT_B)
+#endif
+#ifdef MIXER_CAP_NARROW
 	printf ("LAYOUT_B ");
       if (minfo.caps & MIXER_CAP_NARROW)
+#endif
 	printf ("NARROW ");
       printf ("\n");
       if (verbose < 2)
